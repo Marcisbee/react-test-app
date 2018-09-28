bde4be79e0d6ecf9815f967f4fe357cdc31cb93f
Marcis <marcisbergmanis@gmail.com>
2018-09-28 10:13:24 +0300

    Update prod.js

--- .webpack/prod.js
+++ .webpack/prod.js
@@ -1,9 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
-const Dotenv = require('dotenv-webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const stylusLoader = ExtractTextPlugin.extract("style-loader", "css-loader!stylus-loader");
 
 const resolve = _path => path.resolve(__dirname, _path);
 
@@ -14,7 +11,7 @@ module.exports = {
     resolve("../index.html"),
     resolve("../index.js"),
   ],
-  watch: false,
+  watch: true,
   output: {
     filename: "[name].bundle.js",
     chunkFilename: "[name].bundle.js",
@@ -22,15 +19,6 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.radi/,
-        use: [
-          {
-            loader: 'radi-loader',
-            options: {/* ... */}
-          }
-        ]
-      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -50,40 +38,17 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          // fallback to style-loader in development
-          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'style-loader',
           "css-loader",
           "sass-loader"
         ]
-      },
-      // {
-      //   test: /\.styl$/,
-      //   use: [
-      //     MiniCssExtractPlugin.loader,
-      //     {
-      //       loader: 'css-loader',
-      //       options: { sourceMap: true },
-      //     },
-      //     {
-      //       loader: 'stylus-loader',
-      //       options: { sourceMap: true },
-      //     }
-      //   ]
-      // },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: {
-          loader: "file-loader"
-        }
       }
     ]
   },
   plugins: [
-    new Dotenv(),
     new HtmlWebPackPlugin({
       template: './index.html',
-      filename: './index.html',
-      favicon: './assets/img/favicon.png'
+      filename: './index.html'
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
